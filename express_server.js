06c52cfa40b8f042a0e15d504663a13be1ccc306
stephkri <stephane.krims@hotmail.ca>
2021-08-25 03:16:42 +0000

    Added urls_index, modded to (currently dead) shorten URL link

--- express_server.js
+++ express_server.js
@@ -2,17 +2,24 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
-app.get('/', (req, res) => {
-  res.send('Hello!');
-});
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get('/', (req, res) => {
+  res.send('henlo uwu');
+});
+
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
 
 app.get('/urls.json', (req, res) => {
